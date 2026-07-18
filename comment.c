e0468d5a7d0a3e034af91fdf078ca20dae52226f
adithyavikram2 <adithyavikram2001@gmail.com>
2026-07-18 21:56:39 +0530

    first three programs

---
+++ comment.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    //single line comment
+
+    /* this 
+        is a
+        multi line comment
+    */
+
+    printf("comment program");
+    return 0;
+
+}
---
+++ escape_seq.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    /*
+        escape sequence 
+        example program
+        \n : new line
+        \t : tab key
+
+    */
+
+
+    printf("i\nlove\npizza\n\n");
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+    
+    printf("\n\"comparison is the thief of joy\" : mark twain\n");
+
+    printf("\n\'hello world\'\n");
+
+    printf("\n6\\2 =3\n");
+    return 0;
+}
